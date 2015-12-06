ca12e54e6eb601512c11d279991d28d7464ff96a
lsamaroo <orrinsamaroo@gmail.com>
2015-12-05 20:51:48 -0500

    Add a class to the table.

--- tablesorter.widget-rowIndex.js
+++ tablesorter.widget-rowIndex.js
@@ -13,17 +13,22 @@
 $(function() {
     // add new widget called rowIndex
     $.tablesorter.addWidget({
+    	className: 'row-index',
     	// give the widget a id
-    	id: "rowIndex",
-    	// format is called when on init and when a sorting has finished
+    	id: 'rowIndex',
+    	// format is called on init and when a sorting has finished
     	format: function(table) {			
-    		var rowIndexHeaderExist = $( "thead tr th.row-index", table ).length > 0 ;
+    		if( !$(table).hasClass( this.className ) ){
+    			$(table).addClass( this.className );
+    		}
+    		
+    		var rowIndexHeaderExist = $( 'thead tr th.' + this.className, table ).length > 0 ;
     		if( !rowIndexHeaderExist ){
     			// insert a row index header
-	    		$("thead tr th:first",table).before( "<th class='row-index row-index-created'>&nbsp;</th>" );  			
+	    		$('thead tr th:first',table).before( '<th class="' + this.className + ' ' + this.className +'-created">&nbsp;</th>');  			
     		}
     		 		
-    		var rowIndexColumnExist = $( "tbody tr td.row-index", table ).length > 0 ;
+    		var rowIndexColumnExist = $( 'tbody tr td.' + this.className, table ).length > 0 ;
     		if( rowIndexColumnExist ){
     			this.updateExistingRowIndex( table );
     		}
@@ -39,7 +44,7 @@ $(function() {
     			if( $(rows[i]).hasClass( 'group-header') ){
     				continue;
     			}
-    			$("tbody tr:eq(" + (i) + ") td:first",table).before( "<td class='row-index row-index-created'>" + (index)  + "</td>" );
+    			$('tbody tr:eq(' + (i) + ') td:first',table).before( '<td class="' + this.className + ' ' + this.className + '-created">' + (index)  + '</td>' );
     			index++;
     		}      		
     	},
@@ -51,7 +56,7 @@ $(function() {
     			if( $(rows[i]).hasClass( 'group-header') ){
     				continue;
     			}
-    			$("tbody tr:eq(" + (i) + ") td.row-index",table).html( (index) );
+    			$('tbody tr:eq(' + (i) + ') td.' + this.className,table).html( (index) );
     			index++;
     		}        		
     	}
